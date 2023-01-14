b05ba72c4af215ea35f93fe400e80514ece6c02c
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-14 17:45:16 -0300

    Mostrando página HTML.

--- jogoteca.py
+++ jogoteca.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
 @app.route("/inicio")
 def ola():
-    return "<h1>Hello, world!</h1>"
+    return render_template("lista.html")
 
 
 # A aplicação roda a partir do comando: python <nome_do_arquivo>.py.
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 # Para usar "flask run", defina a variável de ambiente FLASK_APP 
 # para o nome do arquivo: ($Env:FLASK_APP=<file>.py ; flask run --port 80).
