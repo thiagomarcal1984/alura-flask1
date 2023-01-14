ddde561b737d3b97de5de503929139ba8247e8e4
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-14 18:24:30 -0300

    Mostrando atributos na view.

--- jogoteca.py
+++ jogoteca.py
@@ -12,9 +12,10 @@ class Jogo:
 def ola():
     jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
     jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 
-    lista = [ jogo1, jogo2]
-    
+    lista = [ jogo1, jogo2, jogo3]
+
     return render_template("lista.html", titulo="Jogos", jogos=lista)
 
 
